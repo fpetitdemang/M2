7171adff61626982de80b597c9e9469a4e62afb9
Asus <franck@franck-N82JV.(none)>
2013-09-26 11:15:38 +0200

    tuto todolist

---
+++ techno-du-web/play-2.2.0-RC2/MonAppTest/target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/home/franck/M2/techno-du-web/play-2.2.0-RC2/MonAppTest/conf/routes
+// @HASH:fe76ae3b1e48872cef20a9ae251f481633b39d72
+// @DATE:Thu Sep 26 10:23:57 CEST 2013
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+}
+          

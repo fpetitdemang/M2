cafb5f0d8cee853b31cd0ee2c083bd15e70f9119
fpetitdemang <fpetitdemang@r5.etu.info-ufr.univ-montp2.fr>
2013-11-05 12:01:58 +0100

    metaprog : memoclass python

--- meta-prog/memo-class/python/memo-class.py
+++ meta-prog/memo-class/python/memo-class.py
@@ -1,20 +1,13 @@
+"classes sont modeler par la classe -type-"
+
 class MemoClass(type):
      
     """Exemple d'une metaclasse."""
-     
-    def __new__(metacls, nom, bases, dict):
-        """Creation de notre classe."""
-        print("On cree la classe {}".format(nom))
-        metacls.instanceSet = metacls.getInstanceSet
-        return type.__new__(metacls, nom, bases, dict)
 
     def __init__(cls, nom, bases, dict):
         """Constructeur de notre metaclasse, appele quand on cree une classe."""
         type.__init__(cls, nom, bases, dict)
-        cls.instanceSet = list()
-
-    def getInstanceSet(self):
-        return self.instanceSet
+    
 
         
     
@@ -23,3 +16,6 @@ class MemoClass(type):
         
 class MemoObject(MemoClass):
     __metaclass__ = MemoClass
+    
+    "constructeur de MemoObject"
+    "def __init__():"

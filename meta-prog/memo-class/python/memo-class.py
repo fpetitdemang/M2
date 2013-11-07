d44698228006792c02e73ce029f0fb86ffc2bb06
Asus <franck@franck-N82JV.(none)>
2013-11-07 20:41:15 +0100

    metaprog

--- meta-prog/memo-class/python/memo-class.py
+++ meta-prog/memo-class/python/memo-class.py
@@ -1,19 +1,20 @@
 "classes sont modeler par la classe -type-"
 
 class MemoClass(type):
-     
+        
     """Exemple d'une metaclasse."""
     def __new__(metacls, nom, bases, dict):
         """Creation de notre classe."""
-        print("On cree la classe {}".format(nom))
-        "Cree un attribut de classe"
-        dictionnaire = {"instance_set":[]}
-        return type.__new__(metacls, nom, bases, dictionnaire)
+        return type.__new__(metacls, nom, bases, dict)
 
     def __init__(cls, nom, bases, dict):
         """Constructeur de notre metaclasse, appele quand on cree une classe."""
-        cls.instance_set = [cls]
+        cls.instance_set = []
         type.__init__(cls, nom, bases, dict)
+
+    def add_instanceMetacls(cls):
+        cls.instance_set.append(cls)
+ 
 
    
     
@@ -24,18 +25,17 @@ class MemoObject:
     
     "constructeur de MemoObject"
     def __init__(self):
-        print("appel constructeur MemoObject")
         self.add_instance()
 
-    def add_instance():
-        type(self).instance_set.append(self)
+    def add_instance(self):
+        type(self).add_instanceMetacls()
+
+        
 
 
 class Personne(MemoObject):
     pass
 
 
-class Toto:
-    __metaclass__ = MemoClass
-    def __init__(self):
-        print("hello")
+class Auteur(Personne):
+    pass

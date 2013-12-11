985656e0f308c6a12fe904b8193e2d9b0a797298
fpetitdemang <fpetitdemang@v13.etu.info-ufr.univ-montp2.fr>
2013-12-11 10:24:42 +0100

    update

--- meta-prog/memo-class/java/memo-object/src/MemoObject.java
+++ meta-prog/memo-class/java/memo-object/src/MemoObject.java
@@ -1,15 +1,21 @@
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
 
 public class MemoObject {
+    //redefinir l'attribut instanceSet dans toute les classes
+    //redefinir les accesseurs en ecriture
+    //redefinir accesseurs en lecture set<? extends MemoObject> getInstanceSet() -> utilisation des jockers???? attribut immutable
+    //explorer les annotations pour reproduire code
 
 	 private static ArrayList<MemoObject> instanceSet = new ArrayList<MemoObject>(); 
 	 
  
 	 public MemoObject(){
 		 //acces en ecriture de instanceSet
-		 instanceSet.add(this);
+	     //(cast) getinstanceSet.add(this)
+	     instanceSet.add(this);
 	 }
 	 
 	 
